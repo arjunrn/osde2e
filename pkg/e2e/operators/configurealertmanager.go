42eb2b9a77f87832889a7ac98c919edc3289d94e
Arjun Naik <anaik@redhat.com>
2021-04-19 17:23:32 +0200

    Check roles with prefix exist
    Signed-off-by: Arjun Naik <anaik@redhat.com>

--- pkg/e2e/operators/configurealertmanager.go
+++ pkg/e2e/operators/configurealertmanager.go
@@ -24,22 +24,14 @@ var _ = ginkgo.Describe(configureAlertManagerOperators, func() {
 
 	var clusterRoleBindings = []string{}
 
-	var roleBindings = []string{
-		"configure-alertmanager-operator",
-	}
-
-	var roles = []string{
-		"configure-alertmanager-operator",
-	}
-
 	h := helper.New()
 	checkClusterServiceVersion(h, operatorNamespace, operatorName)
 	checkConfigMapLockfile(h, operatorNamespace, operatorLockFile)
 	checkDeployment(h, operatorNamespace, operatorName, defaultDesiredReplicas)
 	checkClusterRoles(h, clusterRoles, true)
 	checkClusterRoleBindings(h, clusterRoleBindings, true)
-	checkRole(h, operatorNamespace, roles)
-	checkRoleBindings(h, operatorNamespace, roleBindings)
+	checkRolesWithNamePrefix(h, operatorNamespace, operatorName, 2)
+	checkRoleBindingsWithNamePrefix(h, operatorNamespace, operatorName, 2)
 	checkUpgrade(helper.New(), "openshift-monitoring", "configure-alertmanager-operator",
 		"configure-alertmanager-operator", "configure-alertmanager-operator-registry")
 })
